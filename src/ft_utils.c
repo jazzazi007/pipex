4db7994d29517008ddfaff5c14ad87fed49632d9
jazzazi007 <maaljazzazi22@eng.just.edu.jo>
2025-03-20 22:51:43 +0300

    Final to submit: norminate the rest of the system

--- src/ft_utils.c
+++ src/ft_utils.c
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moaljazz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 22:48:22 by moaljazz          #+#    #+#             */
+/*   Updated: 2025/03/20 22:50:20 by moaljazz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../include/pipex.h"
 
 char	*get_cmd_assist(char *cmd, char *dir)
 {
-	char *full_path;
+	char	*full_path;
 
 	full_path = malloc(ft_strlen(dir) + ft_strlen(cmd) + 2);
 	if (!full_path)
@@ -14,19 +25,20 @@ char	*get_cmd_assist(char *cmd, char *dir)
 	return (full_path);
 }
 
-void fork1 (int infile, char **ag, char **env, int *pd)
+void	fork1(int infile, char **ag, char **env, int *pd)
 {
-		if (file_errhandle(infile) == 0)
-			first_fork_operate(infile, ag, env, pd);
-		else
-			exit(1);
-    return ;
+	if (file_errhandle(infile) == 0)
+		first_fork_operate(infile, ag, env, pd);
+	else
+		exit(1);
+	return ;
 }
-void fork2 (int outfile, char **ag, char **env, int *pd)
+
+void	fork2(int outfile, char **ag, char **env, int *pd)
 {
-    if (file_errhandle(outfile) == 0)
-				second_fork_operate(outfile, ag, env, pd);
-			else
-				exit(1);
-    return ;
+	if (file_errhandle(outfile) == 0)
+		second_fork_operate(outfile, ag, env, pd);
+	else
+		exit(1);
+	return ;
 }
